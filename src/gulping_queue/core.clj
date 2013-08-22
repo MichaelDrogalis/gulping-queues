8cc2d81270dd1591a8cfdf1c0c473dc1c678a737
Michael Drogalis <mjd3089@rit.edu>
2013-08-22 12:35:56 -0400

    Initial commit.

---
+++ src/gulping_queue/core.clj
@@ -0,0 +1,6 @@
+(ns gulping-queue.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/gulping_queue/core_test.clj
@@ -0,0 +1,7 @@
+(ns gulping-queue.core-test
+  (:require [clojure.test :refer :all]
+            [gulping-queue.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
